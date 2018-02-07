a940d35a1f7ddc680fa9a151800455e68ca6f4e7
Matt Raible <matt.raible@okta.com>
2018-02-07 13:09:34 -0700

    Update Spring Boot to 1.5.10

--- server/src/main/java/com/example/demo/beer/BeerController.java
+++ server/src/main/java/com/example/demo/beer/BeerController.java
@@ -1,4 +1,4 @@
-package com.example.beer;
+package com.example.demo.beer;
 
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,7 +16,7 @@ public class BeerController {
     }
 
     @GetMapping("/good-beers")
-    @CrossOrigin(origins = {"http://localhost:8100","file://"})
+    @CrossOrigin(origins = "http://localhost:4200")
     public Collection<Beer> goodBeers() {
 
         return repository.findAll().stream()
